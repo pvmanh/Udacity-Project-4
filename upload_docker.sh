9efbf82666d0595d46db59cef8404b0b0dce096e
vanmanh.nina <vanmanh.nina@gmail.com>
2023-04-07 09:13:41 +0000

    Update

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+pathdocker="kabuto01/udacity-project-4:v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag udacity-project-4 ${pathdocker}
+echo "Docker ID and Image: $pathdocker"
 
 # Step 3:
 # Push image to a docker repository
+docker push ${pathdocker}
